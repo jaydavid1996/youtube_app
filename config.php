cc199ce03e4bd44c5c033f19bf794390cdb5bb3a
Jay Mark David <jaymarkd@kickseed.cloudstaff.com>
2019-08-16 13:50:50 +0800

    Update config file

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-define(API_KEY, 'AIzaSyDuXcavezwBqd9NbfNvUBnxtrM7qaKnQfI');
-define(SERVER_NAME, 'localhost');
-define(SERVER_USERNAME, 'root');
-define(SERVER_PASSWORD, 'mobile');
-define(SERVER_DATABASE, 'youtube_app');
+define('API_KEY', 'AIzaSyDuXcavezwBqd9NbfNvUBnxtrM7qaKnQfI');
+define('SERVER_NAME', 'localhost');
+define('SERVER_USERNAME', 'root');
+define('SERVER_PASSWORD', 'mobile');
+define('SERVER_DATABASE', 'youtube_app');
